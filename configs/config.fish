0899a6c14069298d49ad09a5794d74b4acb05868
Stefan Partheymüller <s.partheymueller@initics.de>
2020-03-14 16:08:13 +0100

    added functions for fish shell to stage or release changes in a git repo

--- configs/config.fish
+++ configs/config.fish
@@ -11,3 +11,35 @@ alias gb="git branch"
 alias gl="git log --format=oneline --decorate"
 alias gg="git gui"
 alias gdd="git difftool -d"
+
+function gitworkflow
+    set action $argv[1]
+    set source $argv[2]
+    set destination $argv[3]
+
+    set branch (git rev-parse --abbrev-ref HEAD)
+
+    if test $status -ne 0
+        return 1
+    end
+
+    if test "$branch" != "$source"
+        echo "# ERROR:"
+        echo "$action is not allowed for branch '$branch'."
+        echo "You must be on branch '$source'."
+        return 1
+    end
+
+    git checkout $destination
+    git merge $source
+    git push
+    git checkout $source
+end
+
+function stage
+    gitworkflow "Staging" "develop" "staging"
+end
+
+function release
+    gitworkflow "Release" "staging" "master"
+end
