9da0407b0af17974a27dae20bd1df30043d63a89
Stefan Partheymüller <s.partheymueller@initics.de>
2020-03-17 13:48:47 +0100

    added aliases for gitext

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -18,35 +18,5 @@ alias gdd="git difftool -d"
 # Aliases for git extensions (stefanpartheym/fish-git-extensions.git)
 alias gcob="gitext_checkout"
 alias gadd="gitext_add"
-
-function gitworkflow
-    set action $argv[1]
-    set source $argv[2]
-    set destination $argv[3]
-
-    set branch (git rev-parse --abbrev-ref HEAD)
-
-    if test $status -ne 0
-        return 1
-    end
-
-    if test "$branch" != "$source"
-        echo "# ERROR:"
-        echo "$action is not allowed for branch '$branch'."
-        echo "You must be on branch '$source'."
-        return 1
-    end
-
-    git checkout $destination
-    git merge $source
-    git push
-    git checkout $source
-end
-
-function stage
-    gitworkflow "Staging" "develop" "staging"
-end
-
-function release
-    gitworkflow "Release" "staging" "master"
-end
+alias stage="gitext_stage"
+alias release="gitext_release"
